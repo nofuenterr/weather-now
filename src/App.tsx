2ed7b33da0696893bba3b3b7126182d76b5c397e
RR <rrn091301@gmail.com>
2026-01-16 11:25:18 +0800

    feat: add title paragraph to App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ function App() {
 	return (
 		<div className="grid gap-12 px-4 pt-4 pb-12 md:px-6 md:pt-6 md:pb-20 lg:gap-16 lg:px-28 lg:pt-12">
 			<Header />
+			<p className="font-header text-center text-[3.25rem] font-bold text-balance">
+				How’s the sky looking today?
+			</p>
 		</div>
 	);
 }
